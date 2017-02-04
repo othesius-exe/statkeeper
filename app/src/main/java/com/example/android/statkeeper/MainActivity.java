8a2b9946f5d4db39fe290c43b634a0ab0f940c84
Othesius <othesius.exe@gmail.com>
2017-02-04 14:54:21 -0500

    basic layout complete, added some java

--- app/src/main/java/com/example/android/statkeeper/MainActivity.java
+++ app/src/main/java/com/example/android/statkeeper/MainActivity.java
@@ -1,13 +1,80 @@
 package com.example.android.statkeeper;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    int purpleScore;
+    int orangeScore;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    /**
+     *
+     *  Purple team controllers here
+     */
+
+    public void displayPurpleScore(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.purple_score_view);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    public void touchDownPurple(View view) {
+        purpleScore += 6;
+        displayPurpleScore(purpleScore);
+    }
+
+    public void fieldGoalPurple(View view) {
+        purpleScore += 3;
+        displayPurpleScore(purpleScore);
+    }
+
+    public void conversionPurple(View view) {
+        purpleScore += 2;
+        displayPurpleScore(purpleScore);
+    }
+
+    public void fieldGoalPurple(View view) {
+        purpleScore += 1;
+        displayPurpleScore(purpleScore);
+    }
+
+    /**
+     *
+     *  Orange team controllers here
+     */
+    public void displayOrangeScore(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.orange_score_view);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    public void touchDownOrange(View view) {
+        orangeScore += 6;
+        displayOrangeScore(orangeScore);
+    }
+
+    public void fieldGoalOrange(View view) {
+        orangeScore += 3;
+        displayOrangeScore(orangeScore);
+    }
+
+    public void conversionOrange(View view) {
+        orangeScore += 2;
+        displayOrangeScore(orangeScore);
+    }
+
+    public void fieldGoalOrange(View view) {
+        orangeScore += 1;
+        displayOrangeScore(orangeScore);
+    }
+
+
 }
